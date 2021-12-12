0097a55397d954aa2868c5778fdb5defee003612
lukasz <lukasz.stojecki@o2.pl>
2021-12-12 17:09:56 +0100

    * add item projection and h2 db connection

---
+++ src/main/java/pl/stojeckilukasz/cqrstasklincor/projection/ItemProjection.java
@@ -0,0 +1,40 @@
+package pl.stojeckilukasz.cqrstasklincor.projection;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+import pl.stojeckilukasz.cqrstasklincor.event.AddItemEvent;
+import pl.stojeckilukasz.cqrstasklincor.query.model.Item;
+import pl.stojeckilukasz.cqrstasklincor.query.query.GetItemQuery;
+import pl.stojeckilukasz.cqrstasklincor.query.repository.ItemRepository;
+
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ItemProjection {
+
+    private final ItemRepository itemRepository;
+
+    @EventHandler
+    public void on(AddItemEvent addItemEvent) {
+        log.debug("Handling a Add Item command {}", addItemEvent.getClass().getName());
+        Item item = new Item(
+                addItemEvent.getId(),
+                addItemEvent.getName()
+        );
+        itemRepository.save(item);
+    }
+
+    @QueryHandler
+    public Optional<Item> handle(GetItemQuery query) {
+        log.debug("Handling GetItemQuery query: {}", query);
+        return itemRepository.findById(query.getId());
+    }
+
+
+}
