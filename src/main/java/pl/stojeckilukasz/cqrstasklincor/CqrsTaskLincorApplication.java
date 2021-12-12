0c0e2949b72157ecc3911b5c00ff7c3233a78d42
lukasz <lukasz.stojecki@o2.pl>
2021-12-12 16:11:51 +0100

    first commit

---
+++ src/main/java/pl/stojeckilukasz/cqrstasklincor/CqrsTaskLincorApplication.java
@@ -0,0 +1,13 @@
+package pl.stojeckilukasz.cqrstasklincor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsTaskLincorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsTaskLincorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/stojeckilukasz/cqrstasklincor/CqrsTaskLincorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.stojeckilukasz.cqrstasklincor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsTaskLincorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
